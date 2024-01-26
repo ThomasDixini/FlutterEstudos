45b0e642d59decd8dc5e4d550b1387d87f697fa6
Thomás Dixini <thomasdixini@gmail.com>
2024-01-25 21:06:56 -0300

    :sparkles: feat: Adicionando Keyboard Type e dicas do que é pra ser inserido no input (Tipo um placeholder)

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -20,74 +20,84 @@ class _FormScreenState extends State<FormScreen> {
         leading: Container(),
       ),
       body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-              color: Colors.black12,
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(width: 2)),
-          width: 370,
-          height: 670,
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  controller: nameController,
-                  textAlign: TextAlign.center,
-                  decoration: const InputDecoration(
-                      fillColor: Colors.white70,
-                      filled: true,
-                      focusedBorder: OutlineInputBorder()),
+        child: SingleChildScrollView(
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(width: 2)),
+            width: 370,
+            height: 670,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextFormField(
+                    keyboardType: TextInputType.text,
+                    controller: nameController,
+                    textAlign: TextAlign.center,
+                    decoration: const InputDecoration(
+                        hintText: "Nome",
+                        fillColor: Colors.white70,
+                        filled: true,
+                        focusedBorder: OutlineInputBorder()),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  controller: difficultyController,
-                  textAlign: TextAlign.center,
-                  decoration: const InputDecoration(
-                      fillColor: Colors.white70,
-                      filled: true,
-                      focusedBorder: OutlineInputBorder()),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    controller: difficultyController,
+                    textAlign: TextAlign.center,
+                    decoration: const InputDecoration(
+                        hintText: "Dificuldade",
+                        fillColor: Colors.white70,
+                        filled: true,
+                        focusedBorder: OutlineInputBorder()),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  onChanged: (text) {
-                    setState(() {});
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextFormField(
+                    keyboardType: TextInputType.url,
+                    onChanged: (text) {
+                      setState(() {});
+                    },
+                    controller: imageController,
+                    textAlign: TextAlign.center,
+                    decoration: const InputDecoration(
+                        hintText: "Imagem",
+                        fillColor: Colors.white70,
+                        filled: true,
+                        focusedBorder: OutlineInputBorder()),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 72,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 2, color: Colors.blue),
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.blue,
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(imageController.text, fit: BoxFit.cover, errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace){
+                        return Container();
+                      }),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print(nameController);
                   },
-                  controller: imageController,
-                  textAlign: TextAlign.center,
-                  decoration: const InputDecoration(
-                      fillColor: Colors.white70,
-                      filled: true,
-                      focusedBorder: OutlineInputBorder()),
+                  child: const Text("Adicionar"),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 72,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 2, color: Colors.blue),
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.blue,
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.network(imageController.text, fit: BoxFit.cover),
-                  ),
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  print(nameController);
-                },
-                child: const Text("Adicionar"),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
