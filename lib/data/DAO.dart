92e90ab6e5f3dba5b65e10a331428e0160e0123b
Thomás Dixini <thomasdixini@gmail.com>
2024-02-19 21:00:54 -0300

    :sparkles: feat: Criando metodo findAll
    Metodo de acesso ao banco de dados, para retornar todas as tarefas salvas em determinada tabela. Implementado

--- lib/data/DAO.dart
+++ lib/data/DAO.dart
@@ -1,7 +1,10 @@
 import 'package:alura_flutter_curso_1/components/tasks.dart';
+import 'package:alura_flutter_curso_1/data/database.dart';
+import 'package:sqflite/sqflite.dart';
 
 class TaskDAO {
-  static String tableSql = 'CREATE TABLE $_tableName ($_name TEXT, $_difficulty INTEGER, $_image TEXT)';
+  static String tableSql =
+      'CREATE TABLE $_tableName ($_name TEXT, $_difficulty INTEGER, $_image TEXT)';
 
   static const String _tableName = 'taskTable';
   static const String _name = 'name';
@@ -9,6 +12,20 @@ class TaskDAO {
   static const String _image = 'image';
 
   save(Tasks task) async {}
-  findAll() async {}
+
+  Future<List<Tasks>> findAll() async {
+    final Database db = await getDatabase();
+    final List<Map<String, dynamic>> tasks = await db.query(_tableName);
+    return toList(tasks);
+  }
+  List<Tasks> toList(List<Map<String, dynamic>> listOfMap) {
+    final List<Tasks> tasks = [];
+    for (Map<String, dynamic> row in listOfMap) {
+      final task = Tasks(row[_name], row[_difficulty], row[_image]);
+      tasks.add(task);
+    }
+    return tasks;
+  }
+
   find() async {}
 }
